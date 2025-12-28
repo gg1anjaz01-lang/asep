60d2477736fb0731d7fbf122bb1e730d99cc54a5
gg1anjaz01-lang <gg1anjaz01@gmail.com>
2025-12-29 00:18:38 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,9 +9,9 @@ const chalk = require('chalk');
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['6282113821188'] //['628','628'] 2 owner atau lebih
-global.author = 'Nazedev'
-global.botname = 'Hitori Bot'
+global.owner = ['6285716340360,'6283137959070'] //['628','628'] 2 owner atau lebih
+global.author = 'Pupung'
+global.botname = 'Guarden MSv Bot'
 global.packname = 'Bot WhatsApp'
 global.listprefix = ['+','!','.']
 
@@ -90,3 +90,4 @@ fs.watchFile(file, () => {
 	delete require.cache[file]
 	require(file)
 });
+
